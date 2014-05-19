426c9d78706a23045bc8f4ac46b7002f4b9c50da
Anh Do <anh.do@lonelyplanet.com.au>
2014-05-19 11:04:27 +1000

    simple base class

---
+++ lib/document_reader.rb
@@ -0,0 +1,55 @@
+class PlainTextReader < DocumentReader
+  def self.can_read?(path)
+    /.*\.txt/ =~ path
+  end
+
+  def initialize(path)
+    @path = path
+  end
+
+  def read(path)
+    File.open(path) do |f|
+      title = f.readline.chomp
+      author = f.readline.chomp
+      content = f.read.chomp
+      Document.new( title, author, content )
+    end
+  end
+end
+
+class YAMLReader < DocumentReader
+  def self.can_read?(path)
+    /.*\.yaml/ =~ path
+  end
+
+  def initialize(path)
+    @path = path
+  end
+
+  def read(path)
+
+  end
+end
+
+class XMLReader < DocumentReader
+  def self.can_read?(path)
+    /.*\.xml/ =~ path
+  end
+
+  def initialize(path)
+    @path = path
+  end
+
+  def read(path)
+
+  end
+end
+
+class DocumentReader
+
+  class << self
+    attr_reader :reader_classes
+  end
+
+  @reader_classes = []
+end
