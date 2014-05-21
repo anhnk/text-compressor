cd926925a0ea9b82301f0dc14deea30ead0af77d
Anh Do <dotuanhoanganh@gmail.com>
2014-05-21 22:03:16 +1000

    ignore swp files

--- lib/document_reader.rb
+++ lib/document_reader.rb
@@ -52,4 +52,22 @@ class DocumentReader
   end
 
   @reader_classes = []
+
+  def self.read(path)
+    reader = reader_for(path)
+    return nil unless reader
+    reader.read(path)
+  end
+
+  def self.reader_for(path)
+    reader_class = DocumentReader.reader_classes.find do |klass|
+      klass.can_read?(path)
+    end
+    return reader_class.new(path) if reader_class
+    nil
+  end
+
+  def self.inheried(subclass)
+    DocumentReader.reader_classes << subclass
+  end
 end
